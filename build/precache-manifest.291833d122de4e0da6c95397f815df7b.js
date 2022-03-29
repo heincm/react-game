f9adc2fdd58be24adfe53c321f2da4daa296508f
Chris <chris.m.hein@gmail.com>
2022-03-28 23:45:18 -0400

    working

---
+++ build/precache-manifest.291833d122de4e0da6c95397f815df7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9e8a736ccd214b2e182",
+    "url": "/react-game/static/css/main.99f9c478.chunk.css"
+  },
+  {
+    "revision": "f9e8a736ccd214b2e182",
+    "url": "/react-game/static/js/main.a13f8b29.chunk.js"
+  },
+  {
+    "revision": "3a038c117699a7a341e8",
+    "url": "/react-game/static/js/runtime~main.ef933c85.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "0fb828ed4e2122f8794a5e49ae13b959",
+    "url": "/react-game/index.html"
+  }
+];
